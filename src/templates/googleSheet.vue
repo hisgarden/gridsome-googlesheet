6d11655967208f88824ad9ccf5ed8dc642dcf399
Jin Wen <jin.wen@hisgarden.org>
2021-04-05 23:23:14 -0500

    add template googleSheet.vue

---
+++ src/templates/googleSheet.vue
@@ -0,0 +1,15 @@
+<template>
+  <layout>
+    <div>{{$page.googleSheet.title}}</div>
+    <div>{{$page.googleSheet.body}}</div>
+  </layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+  googleSheet (path: $path) {
+    title
+    body
+  }
+}
+</page-query>
