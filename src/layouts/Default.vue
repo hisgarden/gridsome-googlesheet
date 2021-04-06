f3f279d5d8607cab4263079da5f319f6395bcc4b
Jin Wen <jin.wen@hisgarden.org>
2021-04-06 17:07:06 -0500

    added vuetify CSS framework for style

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <v-app>
     <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
@@ -9,8 +9,10 @@
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
-    <slot/>
-  </div>
+    <v-main>
+      <slot/>
+    </v-main>
+  </v-app>
 </template>
 
 <static-query>
@@ -20,31 +22,3 @@ query {
   }
 }
 </static-query>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
