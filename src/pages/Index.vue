2b045e5cd3b62e4f1047dfbb959fbd07b26bee19
Jin Wen <jin.wen@hisgarden.org>
2021-04-06 21:19:35 -0500

    trying out v2 of the googleSpre

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,22 @@
-<script src="http://localhost:8098"></script>
 <template>
   <Layout>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <section>
+      <v-container>
+        <v-row>
+   <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
+        </v-row>
+      </v-container>
+    </section>
+ 
+
+
 
     <h1>Hello UC Master Gardeners!</h1>
     <div v-for="page in $page.allGoogleSheet.edges" :key="page.id">
       <strong><em>{{ page.node.Scientific_Name }}</em></strong>
-      <!--Commonly Known as: {{ page.node.Common_Name }}-->
+      -- Commonly Known as: {{ page.node.Common_Name }}
       located at {{ page.node.Location}}
 
     </div>
@@ -23,17 +31,16 @@ query {
     edges {
       node {
         id
-        title
         Scientific_Name
         Common_Name
         Description
-        Bloom_Season_
+        Bloom_Season
         Water_Needs
         Exposure
         Pruning_Needs
         Location
-        Size__width_
-        Size__height__
+        Size_width
+        Size_height
         Type
       }
     }
