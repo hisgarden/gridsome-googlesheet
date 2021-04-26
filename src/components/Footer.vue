ec2cef8f8eb7221fe6af0fc5dadd3e602f53b982
Jin Wen <jin.wen@hisgarden.org>
2021-04-25 21:19:52 -0700

    fixed the footer for now

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,13 @@
 <template>
-  <v-footer dark padless class="mt-5">
+  <v-footer absolute dark padless class="mt-5">
     <v-card
       flat
       tile
       class="primary white--text text-center align-self-md-stretch"
       width="100%"
     >
-      <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
-      </v-card-text>
-
-      <v-card-text class="white--text pt-0">
-        info@website.com | illustrations from
-
-      </v-card-text>
-
-      <v-divider></v-divider>
-
       <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        &copy; {{ new Date().getFullYear() }} — <strong>Regents of the University of California</strong>
       </v-card-text>
     </v-card>
   </v-footer>
