62511eb4def7aaf0e83783899cfcf2842a8a1a97
Jin Wen <jin.wen@hisgarden.org>
2021-05-04 23:22:08 -0700

    Improve all plant listing to include action buttons

--- src/components/AllPlants.vue
+++ src/components/AllPlants.vue
@@ -6,51 +6,63 @@
         <v-col>
           <g-image alt="Prototype image" src="~/Arctostaphylos_Sunset-SunsetManzanita.png" width="300" />
         </v-col>
-        <v-col sm="6" class="d-flex flex-column justify-center">
+        <v-col sm="6" >
           <div v-for="page in $page.allGoogleSheet.edges" :key="page.node.Scientific_Name">
 
 
           <v-card
             max-width="400"
             class="mx-auto"
-            outlined
+            elevation="4"
+            shaped
           >
-<!--             <v-img
-              class="white--text align-end"
-              height="200px"
-              src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
-            >
-            
-            </v-img> -->
+
             <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
+                
                 <v-avatar
                     class="ma-3"
                     size="194"
                     tile
-                  >
-                    <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
+                >
+                  <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg" alt="page.node.Scientific_Name"></v-img>
                 </v-avatar>
-                </div>
-
-                icon_native, icon_bee
-              </div>
+                
+                
+                icon_native, icon_bee, icon_water_need, icon_exposure
+                 
+            </div>
  
                 <v-card-title>{{ page.node.Common_Name }}</v-card-title>
                 <v-card-subtitle>({{ page.node.Scientific_Name }})</v-card-subtitle>
                <v-card-text>
-                  {{ page.node.Description }}
+                  {{ page.node.Description }}<br>
+                  Located at: {{ page.node.Location}}
               </v-card-text>
+             
               <v-card-actions>
-                
                 <v-btn
+                  text
                   color="success"
                   outlined
                 >
-                  <v-icon></v-icon>
-                  {{ page.node.Location}}
+                  <v-icon>mdi-magnify</v-icon>Learn more
+                </v-btn>  
+                <v-spacer></v-spacer>
+  
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
                 </v-btn>
-            </v-card-actions>
+  
+                <v-btn icon>
+                  <v-icon>mdi-bookmark</v-icon>
+                </v-btn>
+  
+                <v-btn icon>
+                  <v-icon>mdi-share-variant</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+      
           </v-card>
           </div>
         </v-col>
